d410097f9bca70dcec942fd98a3569399ead0591
JongseoLee <jsclubb@gmail.com>
2016-07-15 13:31:34 +0900

    2016_07_15_3

---
+++ com.js.ens.transformation/userModule/individual/f7/wr_f4_f7.py
@@ -0,0 +1,35 @@
+#
+#
+from py_mentat import *
+from py_post import *
+
+
+
+def main():   
+   wr_crown=py_get_float("wr_crown") 
+   print "Work Roll Crown is ",wr_crown 
+   if abs(wr_crown) <= 0.0000000001 : 
+      py_send("*exec_proc wr_f4_f7.proc ")
+      print " Work Roll Crown is not applied "
+   elif wr_crown > 0.0000000001 :
+      py_send("*exec_proc wr_f4_f7_pcrown.proc ")
+      print " Plus Work Roll Crown is applied "
+   elif wr_crown < -0.0000000001 :
+      py_send("*exec_proc wr_f4_f7_mcrown.proc ")
+      print " Minus Work Roll Crown is applied "
+   else :
+      print " Crown is not detected "   
+   print " Finished Work Roll Crown Definition "
+   return     
+
+   
+if __name__ == '__main__':
+    py_connect("",40007)
+    main()
+    py_disconnect()
+
+
+
+
+
+
